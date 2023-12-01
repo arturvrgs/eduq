8d5dcb4a4335cdb6193de675bffed5fde19f5cdd
Bowie <124817558+arturvrgs@users.noreply.github.com>
2023-12-01 18:30:47 -0300

    first commit

---
+++ tailwindcfg.js
@@ -0,0 +1,20 @@
+
+tailwind.config = {
+    theme: {
+        extend: {
+        colors: {
+            roxo: {
+                claro: '#2E1065',
+                escuro: '#1F1A24'           
+            },
+            fundo: {
+                claro: '#EBEBEB',
+                escuro: '#121212'
+            },
+        },
+        fontFamily: {
+            montserrat: ['Montserrat', 'sans-serif']
+        }
+      }
+    }
+}
